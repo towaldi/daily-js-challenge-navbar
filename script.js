7a539a117340b2b0ec6e8894746b4ac7c9d684c4
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-13 12:14:44 +0100

    final version

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ const tabList = document.getElementById('tab-list');
 console.log(menuBtn);
 console.log(tabList);
 
+// Checking window size at initialization
+
 
 /**
  * Show tab list
+ * -> click on button
  */
 
 menuBtn.addEventListener('click', function() {
-    tabList.classList.toggle('tab-list');
+    tabList.classList.toggle('d-none');
 
-    if (tabList.classList.contains('tab-list')) {
-        tabList.style.display = 'flex';
-        console.log(tabList.classList);
-    } else {
-        tabList.style.display = 'none';
-    }
-})
+    // if (tabList.classList.contains('tab-list')) {
+    //     tabList.style.display = 'flex';
+    //     console.log(tabList.classList);
+    // } else {
+    //     tabList.style.display = 'none';
+    // }
+});
