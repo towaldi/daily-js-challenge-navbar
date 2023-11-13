5d21680ed0095377a7480ff3d487ac3bcdfabbaf
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-13 08:17:46 +0100

    general setup

---
+++ data.js
@@ -0,0 +1,25 @@
+let REVIEW_DATA = [
+    {
+        id: 0,
+        img: './assets/img/person_1.jpg',
+        name: 'Susan Smith',
+        profession: 'Web Developer',
+        quote: '"Unlocking efficiency, one click at a time—where software meets simplicity."'
+    }, 
+    {
+        id: 1,
+        img: './assets/img/person_2.jpg',
+        name: 'Emma Watson',
+        profession: 'Scientist',
+        quote: '"In the world of code, our application speaks volumes—making complexity an art form."'
+
+    },
+    {
+        id: 2,
+        img: './assets/img/person_3.jpg',
+        name: 'John Dow',
+        profession: 'Software Engineer',
+        quote: '"Transforming ideas into innovation, our software is the bridge between imagination and implementation."'
+
+    }
+];
